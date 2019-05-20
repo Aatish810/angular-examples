02dc016f2d26770866c7878e650891464cce8b23
Aatish Sharma <aatish.sharma28@gmail.com>
2019-05-20 14:46:44 +0530

    changing to angular 6

--- src/app/global-components/global-error-handler/global-error-handler.ts
+++ src/app/global-components/global-error-handler/global-error-handler.ts
@@ -2,7 +2,8 @@ import { ErrorHandler, Injectable, Injector, Inject } from '@angular/core';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/throw';
+import { _throw } from 'rxjs/observable/throw';
+// import { throwError } from 'rxjs';
 import * as StackTrace from 'stacktrace-js';
 
 import { AppError } from './app-error';
@@ -10,6 +11,7 @@ import { BadInputError } from './bad-input-error';
 import { NotFoundError } from './not-found-error';
 import { InternalServerError } from './server-error';
 import { HTTPError } from './http-error.model';
+import { throwError } from 'rxjs';
 
 
 
@@ -36,11 +38,7 @@ export class GlobalErrorHandler extends ErrorHandler {
             return sf.toString();
           }).join('\n');
         console.log('%c STACKTRACEJS INFO !!! ', 'background: #5bc0de; color: #FFF', { error, message, url, stack: stackString });
-        // log on the server
-        // loggingService.log({ error, message, url, stack: stackString });
-      });
-
-      throw error;
+      }).catch(() => { throw error; });
     } else {
       super.handleError(error);
     }
@@ -54,7 +52,7 @@ export class GlobalHTTPErrorHanlder {
 
   }
 
-  public handleError(error: HTTPError) {
+  public handleError(error: any) {
     let errMsg: string;
     if (error.error instanceof HttpErrorResponse) {
       const body = error || '';
@@ -63,15 +61,37 @@ export class GlobalHTTPErrorHanlder {
       console.error('%c ERROR !!! ', 'background: #FF0000; color: #FFF', errMsg);
     } else {
       if (error.status === 404) {
-        return Observable.throw(new NotFoundError());
+        // return Observable.throw(new NotFoundError(error));
+        // console.log('404 not found error');
+        const err = new NotFoundError(error);
+        const obj = { message: err['statusText'] };
+        return throwError(obj);
+        // return throwError(new NotFoundError(error));
       } else if (error.status === 400) {
-        return Observable.throw(new BadInputError(error.error.json()));
+        // return Observable.throw(new BadInputError(error.error.json()));
+        // console.log('BadInputError error');
+        // return throwError(new BadInputError(error.error.errors[0]));
+        return throwError(error.error.errors[0]);
       } else if (error.status === 500) {
-        return Observable.throw(new InternalServerError());
+        // console.log('Internal server error');
+        // return Observable.throw(new InternalServerError(error));
+        const err = new InternalServerError(error);
+        const obj = { message: err['statusText'] };
+        return throwError(obj);
+        // return throwError(new InternalServerError(error));
       } else {
-        return Observable.throw(new AppError(error));
+        // console.log('App error');
+        // return Observable.throw(new AppError(error));
+        const err = new AppError(error);
+        const obj = { message: err['statusText'] };
+        // return throwError(new AppError(error));
+        return throwError(obj);
+        // const error1 = new Error('BOOM!');
+        // return throwError(error1);
       }
+
     }
-    return Observable.throw(error);
+    // return Observable.throw(error);
+    // return throwError(error);
   }
 }

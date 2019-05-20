2b41a25dda05853a4dbe92e4a79bd4fd15d02be2
Aatish Sharma <aatish.sharma28@gmail.com>
2019-05-20 14:48:04 +0530

    adding chnagesd to rxjs 6

--- src/app/global-components/http-interceptor/app-http-common.service.ts
+++ src/app/global-components/http-interceptor/app-http-common.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
+import { map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 
 import { GlobalHTTPErrorHanlder } from '../global-error-handler/global-error-handler';
@@ -14,39 +14,38 @@ export class HTTPCommonService {
     constructor(private http: HttpClient, private globalHTTPErrorHanlder: GlobalHTTPErrorHanlder) { }
 
     get(url: string): Observable<any> {
-        return this.http.get(url)
-            .map(res => this.extractData(res))
-            .catch(err => this.globalHTTPErrorHanlder.handleError(err));
+        return this.http.get(url).pipe(map(res => this.extractData(res)))
+            .pipe(catchError(err => this.globalHTTPErrorHanlder.handleError(err)));
     }
 
     post(url: string, payload): Observable<any> {
 
         return this.http.post(url, payload)
-            .map(this.extractData)
-            .catch(err => this.globalHTTPErrorHanlder.handleError(err));
+            .pipe(map(this.extractData))
+            .pipe(catchError(err => this.globalHTTPErrorHanlder.handleError(err)));
     }
 
     // update the entire payload
     put(url: string, payload): Observable<any> {
 
         return this.http.put(url, payload)
-            .map(this.extractData)
-            .catch(err => this.globalHTTPErrorHanlder.handleError(err));
+            .pipe(map(this.extractData))
+            .pipe(catchError(err => this.globalHTTPErrorHanlder.handleError(err)));
     }
 
     // update the few properties of the payload
     patch(url: string, payload): Observable<any> {
 
         return this.http.patch(url, payload)
-            .map(this.extractData)
-            .catch(err => this.globalHTTPErrorHanlder.handleError(err));
+            .pipe(map(this.extractData))
+            .pipe(catchError(err => this.globalHTTPErrorHanlder.handleError(err)));
     }
 
 
     delete(url: string): Observable<any> {
         return this.http.delete(url)
-            .map(res => res)
-            .catch(err => this.globalHTTPErrorHanlder.handleError(err));
+            .pipe(map(res => res))
+            .pipe(catchError(err => this.globalHTTPErrorHanlder.handleError(err)));
     }
 
 
